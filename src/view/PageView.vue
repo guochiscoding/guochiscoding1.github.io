b94b62728365aa147e0bde5e7e493d4778038548
guochiscoding <15021806646@163.com>
2024-05-29 23:55:23 +0800

    dist

--- src/view/PageView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div id="container">
-    <router-view />
-  </div>
-</template>
-
-<script setup name="PageView"></script>
-
-<style scoped></style>
--- src/view/Software_bigData.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div id="Software">大数据管理系统</div>
-</template>
-<script setup name="bigData"></script>
-
